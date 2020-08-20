20bf0ba21984fceda5f3de4f163615d4c28a0038
canberk5617 <canberk_nurcan@hotmail.com>
2020-08-21 01:39:00 +0300

    First create of codes

---
+++ SerializationbdDeserilization/Person.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Universe
+{
+    [Serializable]
+    class Person : ISerializable
+    {
+        public string  Name{ get; set; }
+        public string  Surname{ get; set; }
+        public int     Age{ get; set; }
+        public double  Weight{ get; set; }
+        public double  Height{ get; set; }
+        public string  Job { get; set; }
+
+        IFormatter bf;
+
+        public Person()
+        {
+
+        }
+        public Person(string Name, string Surname, int Age,double Weight,double Height,string Job)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+            this.Weight = Weight;
+            this.Height = Height;
+            this.Job = Job;
+
+        }
+
+        public string[] Tostring()
+        {
+            Console.WriteLine("Name    = " + Name);
+            Console.WriteLine("Surname = " + Surname);
+            Console.WriteLine("Age     = " + Age.ToString());
+            Console.WriteLine("Weight  = " + Weight.ToString());
+            Console.WriteLine("Height  = " + Height.ToString());
+            Console.WriteLine("Job     = " + Job);
+
+            return new string[] {Name,Surname,Age.ToString(),Weight.ToString(),Height.ToString(),Job.ToString() };
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(Name, "Name");
+            info.AddValue(Surname, "Surname");
+            info.AddValue(Convert.ToString(Age), "Age");
+            info.AddValue(Convert.ToString(Weight), "Weight");
+            info.AddValue(Convert.ToString(Height), "Height");
+            info.AddValue(Job, "Job");
+
+            Console.WriteLine(info.FullTypeName);
+
+        }
+
+        public void Serialize()
+        {
+
+            Person Temporary;
+
+            string JsonString;
+             bf = new BinaryFormatter();
+
+            //Json Serialize
+            Stream JsonStream = File.Open(@"People\" + Name + " " + Surname + ".Json", FileMode.OpenOrCreate);
+            var JsonOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            JsonString = JsonSerializer.Serialize(this,JsonOptions);
+            StreamWriter sw = new StreamWriter(JsonStream);
+            sw.WriteLine(JsonString);
+            sw.Close();
+            
+
+            JsonStream.Close();
+
+            //Binary Serialize
+            Stream BinaryStream = File.Open(@"People\" + Name + " " + Surname + ".dat", FileMode.OpenOrCreate);
+            bf.Serialize(BinaryStream,this);
+
+
+            BinaryStream.Close();
+            //Temporary.ToString();
+
+            Console.WriteLine();
+        }
+
+        public void Deserilaze()
+        {
+            Console.WriteLine("          Beginning of the " + Name);
+
+            Person Temporary = null;
+            string TemporaryString = string.Empty;
+
+            //Json deserialize
+            Console.WriteLine();
+            Console.WriteLine("  According to json file: \n");
+            string Jsonpath = @"People\" + Name + " " + Surname + ".Json";
+            if (File.Exists(Jsonpath))
+            {
+                try
+                {
+
+                TemporaryString = File.ReadAllText(Jsonpath);
+                Temporary = JsonSerializer.Deserialize<Person>(TemporaryString);
+            Temporary.Tostring(); // If you delete this line, Console doesn't write properties. I do not know why?
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occured while deserializing {0}  {1}.Json file, Error message is here \n {2}",Name,Surname,e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to find " + Name + " " + Surname + ".Json");
+            }
+
+            //Binary Deserilization
+            Console.WriteLine();
+            Console.WriteLine("  According to the dat file: \n");
+            string DatPath = @"People\" + Name + " " + Surname + ".Json";
+            if (File.Exists(DatPath))
+            {
+                try
+                {
+                    using (FileStream BinaryStream = new FileStream(DatPath, FileMode.Open)) {
+                        bf = new BinaryFormatter();
+                    Temporary = (Person)bf.Deserialize(BinaryStream);
+                        Temporary.Tostring();
+                    }
+                }
+                catch (Exception k)
+                {
+                    Console.WriteLine("Oops looks like a error is occured because of unknown fucking error1!!!!! Anyways error message:\n {0}", k.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to find " + Name + " " + Surname + ".dat");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("          End of the " + Name);
+            Console.WriteLine();
+        }
+
+
+        }
+    }
+
+
---
+++ SerializationbdDeserilization/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Universe;
+
+namespace Main
+{
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////    Author: Canberk Pitirli
+    ///////////    Version: 1.0v
+    ///////////    Released date: 20//08/2020
+    ///////////    
+    ///////////    Application Name: Serialization/Deserialization
+    ///////////    Purpose: Learning
+    ///////////    Description: This application demonstrate that how to use Serialization and Deserialization in json and binary(dat) files. Firstly I created a Person named class and added some 
+    ///////////    properties like Name, Surname, Age etc... Afterwards, I added serialize and Deserialize for hiding datas in folders so that application creates a folder named "People" and adds
+    ///////////    people which you utialized Serialize method on them. I'll code my own serializor more different than other serializor in the future...
+    //////////
+    //////////     For more information: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/serialization/
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Canberk = new Person("Canberk","Pitirli",19,89.0,1.86,"Scientist");
+            Person Marcus = new Person
+            {
+                Name = "Marcus",
+                Surname = "Person",
+                Age = 38,
+                Weight = 117.41,
+                Height = 1.79,
+                Job = "Game Programmer"
+            };
+
+
+            Marcus.Serialize();
+            Marcus.Deserilaze();
+            Canberk.Serialize();
+            Canberk.Deserilaze();
+
+            Console.ReadLine();
+        }
+    }
+}
